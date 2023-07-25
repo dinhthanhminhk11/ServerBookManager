427256dc3641f8ccc108861307238511a7e7b0ab
minhdinh <minhdinhthanh@vccorp.vn>
2023-07-25 11:17:22 +0700

    update server

--- src/controller/loanslip.js
+++ src/controller/loanslip.js
@@ -94,94 +94,96 @@ class Loanslip {
     async getPopularBooks(req, res) {
         try {
             const result = await loanslip.aggregate([
-              {
-                $match: { isPay: true },
-              },
-              {
-                $group: {
-                  _id: "$idBook",
-                  size: { $sum: 1 },
+                {
+                    $match: { isPay: true },
                 },
-              },
-              {
-                $lookup: {
-                  from: "books", // Tên collection của bảng Book (collection)
-                  localField: "_id",
-                  foreignField: "_id",
-                  as: "bookInfo",
+                {
+                    $group: {
+                        _id: "$idBook",
+                        size: { $sum: 1 },
+                    },
                 },
-              },
-              {
-                $project: {
-                  _id: 0,
-                  nameBook: { $arrayElemAt: ["$bookInfo.name", 0] },
-                  size: 1,
+                {
+                    $lookup: {
+                        from: "books", // Tên collection của bảng Book (collection)
+                        localField: "_id",
+                        foreignField: "_id",
+                        as: "bookInfo",
+                    },
                 },
-              },
+                {
+                    $project: {
+                        _id: 0,
+                        nameBook: { $arrayElemAt: ["$bookInfo.name", 0] },
+                        size: 1,
+                    },
+                },
             ]);
-        
+
             if (result.length > 0) {
-              res.status(200).json(result);
+                res.status(200).json(result);
             } else {
-              res.status(404).json({ message: "No popular books found." });
+                res.status(404).json({ message: "No popular books found." });
             }
-          } catch (error) {
+        } catch (error) {
             console.error(error);
             res.status(500).json({ message: "Server error." });
-          }
+        }
     }
 
-    async  getREvene(req, res) {
+    async getREvene(req, res) {
         try {
-            const startDate = req.params.startDate; // Định dạng timeLong
-            const endDate = req.params.endDate; // Định dạng timeLong
-        
-            console.log(startDate)
-            console.log(endDate)
+            const startDate = parseInt(req.query.startDate);
+            const endDate = parseInt(req.query.endDate);
+
             const result = await loanslip.aggregate([
-              {
-                $match: {
-                  isPay: true,
-                  $or: [
-                    { borrowedDate: { $gte: startDate, $lte: endDate } },
-                    { payDay: { $gte: startDate, $lte: endDate } },
-                  ],
+                {
+                    $match: {
+                        $or: [
+                            { $and: [{ isPay: false }, { borrowedDate: { $gte: startDate, $lte: endDate } }] },
+                            { $and: [{ isPay: true }, { payDay: { $gte: startDate, $lte: endDate } }] },
+                        ],
+                    },
                 },
-              },
-              {
-                $group: {
-                  _id: "$idBook",
-                  size: { $sum: 1 },
+                {
+                    $group: {
+                        _id: null,
+                        unpaidAmount: {
+                            $sum: {
+                                $cond: [{ $and: [{ $eq: ["$isPay", false] }, { $gte: ["$borrowedDate", startDate] }, { $lte: ["$borrowedDate", endDate] }] }, 1, 0],
+                            },
+                        },
+                        amountPaid: {
+                            $sum: {
+                                $cond: [{ $and: [{ $eq: ["$isPay", true] }, { $gte: ["$payDay", startDate] }, { $lte: ["$payDay", endDate] }] }, 1, 0],
+                            },
+                        },
+                        totalMoney: {
+                            $sum: {
+                                $cond: [{ $eq: ["$isPay", true] }, "$price", 0],
+                            },
+                        },
+                    },
                 },
-              },
-              {
-                $lookup: {
-                  from: "books", // Tên collection của bảng Book (collection)
-                  localField: "_id",
-                  foreignField: "_id",
-                  as: "bookInfo",
-                },
-              },
-              {
-                $project: {
-                  _id: 0,
-                  nameBook: { $arrayElemAt: ["$bookInfo.name", 0] },
-                  size: 1,
-                },
-              },
             ]);
-        
+
             if (result.length > 0) {
-              res.status(200).json(result);
+                const combinedResult = {
+                    unpaidAmount: result.reduce((acc, item) => acc + item.unpaidAmount, 0),
+                    amountPaid: result.reduce((acc, item) => acc + item.amountPaid, 0),
+                    totalMoney: result.reduce((acc, item) => acc + item.totalMoney, 0),
+                };
+
+                res.status(200).json(formatResponseSuccess(combinedResult, true, 'Lấy thành công'));
             } else {
-              res.status(404).json({ message: "No popular books found." });
+                res.status(200).json(formatResponseSuccess(null, false, 'Không tìm thấy sách phổ biến.'));
             }
-          } catch (error) {
+        } catch (error) {
             console.error(error);
             res.status(500).json({ message: "Server error." });
-          }
-      }
-      
+        }
+    }
+
 }
 
 export default new Loanslip();
